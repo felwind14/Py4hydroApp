2a9b8ba85fc82e464e862574ebafe67dbfa1cd5a
felwind14 <felipe.fonsecaa14@gmail.com>
2023-10-03 18:11:53 +0200

    Making an interection script

--- 3_MapAlPcrtut/1_MapAlgebra/z_3distance_Ev.py
+++ 3_MapAlPcrtut/1_MapAlgebra/z_3distance_Ev.py
@@ -1,46 +1,70 @@
 # Import the library
 from pcraster import *
 
-def WithinDistance(raster,maxdistance):
+def withinDistance(raster,maxdistance):
     ResultWithinDistance = spreadmaxzone(raster, 0, 1, maxdistance)
     return ResultWithinDistance
     
-def BeyondDistance(raster,mindistance):
+def beyondDistance(raster,mindistance):
     ResultBeyondDistance = ~ spreadmaxzone(raster, 0, 1, mindistance)
     return ResultBeyondDistance
 
-# Change to data folder if needed
-os.chdir("./data")
+def intersect(*args):   
+    if not args:
+        return None
+    # Initialize the result raster with the first raster in rasters
+    inters = args[0]
+    # Iterate through the remaining rasters and perform the intersection
+    for raster in args[1:]:
+        inters = inters & raster
+    return inters
 
-# Read all input maps
-Buildings = readmap("fbuildg.map")
-Roads = readmap("froads.map")
-GWLevel = readmap("fgwlevel.map")
-DTM = readmap("fdtm.map")
+def visual(raster):
+    # Write result to disk and visualise
+    report(raster,"faccessiblewells.map")
+    plot(raster,labels={0:"Not accessible",1:"Accessible"},title="Wells",filename=None)
 
-# Set thresholds for conditions
-DistanceCondition1 = 150
-DistanceCondition2 = 300
-DepthCondition3 = 40
+def main():
+    # Change to data folder if needed
+    os.chdir("./data")
+    # Read all input maps
+    Buildings = readmap("fbuildg.map")
+    Roads = readmap("froads.map")
+    GWLevel = readmap("fgwlevel.map")
+    DTM = readmap("fdtm.map")
 
-# Condition 1: Wells within X Meters of Houses or Roads
-Houses = Buildings == 1
-Houses150m = WithinDistance(Houses,DistanceCondition1)
+    # Set thresholds for conditions
+    DistanceCondition1 = 150
+    DistanceCondition2 = 300
+    DepthCondition3 = 40
 
-IsRoad = Roads != 0
-Roads150m = WithinDistance(IsRoad,DistanceCondition1)
+    # Condition 1: Wells within X Meters of Houses or Roads
+    Houses = Buildings == 1
+    Houses150m = withinDistance(Houses,DistanceCondition1)
 
-# Condition 2: No Industry, Mine, or Landfill within X Meters from Wells
-Industry = lookupboolean("industry.tbl", Buildings)
-IndustryMin300m = BeyondDistance(Industry,DistanceCondition2)
+    IsRoad = Roads != 0
+    Roads150m = withinDistance(IsRoad,DistanceCondition1)
 
-# Condition 3: Wells Less than X Meters Deep
-WellDepth = DTM - GWLevel
-NotDeep = WellDepth < DepthCondition3
+    # Condition 2: No Industry, Mine, or Landfill within X Meters from Wells
+    Industry = lookupboolean("industry.tbl", Buildings)
+    IndustryMin300m = beyondDistance(Industry,DistanceCondition2)
 
-# Combine conditions
-AccessibleWells = Houses150m & Roads150m & IndustryMin300m & NotDeep
+    # Condition 3: Wells Less than X Meters Deep
+    WellDepth = DTM - GWLevel
+    NotDeep = WellDepth < DepthCondition3
 
-# Write result to disk and visualise
-report(AccessibleWells,"faccessiblewells.map")
-plot(AccessibleWells,labels={0:"Not accessible",1:"Accessible"},title="Wells",filename=None)
+    # Combine conditions
+    AccessibleWells = intersect(Houses150m, Roads150m, IndustryMin300m, NotDeep)
+    #Visualization
+    visual(AccessibleWells)
+
+
+
+if __name__ == "__main__":
+    main()    
+
+
+
+
+
+
